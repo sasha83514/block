1200e2aa632bb8c1183689aa4235812531b59ef5
sasha83514 <115151417+sasha83514@users.noreply.github.com>
2022-11-21 12:33:26 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+function creatBlock() {
+    let box = document.querySelector('.box')
+    let newBlock = document.createElement('div')
+    newBlock.classList = ('mini')
+    box.append(newBlock)
+}
